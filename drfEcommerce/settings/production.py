e2c2d49b59b234b0179e3d27328001ed7f846d27
amirhosein <amirhoseinsavedarbandsary@gmail.com>
2024-07-12 14:28:47 +0330

    add the initialization of the project with the base files

---
+++ drfEcommerce/settings/production.py
@@ -0,0 +1,14 @@
+from .base import * 
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALLOWED_HOSTS = ['*']
